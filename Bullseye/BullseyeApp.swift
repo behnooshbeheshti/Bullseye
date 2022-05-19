36f4398c8ad36c1739b29e0ca26ac1a9513abe23
Behnoosh <behnoosh.beheshti@gmail.com>
2022-05-19 17:23:13 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Mohsen Hossein pour on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
