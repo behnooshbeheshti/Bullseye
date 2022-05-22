59e8f3132f3d8b74afaa8980d49273b6aa7fdf2b
Behnoosh <behnoosh.beheshti@gmail.com>
2022-05-22 20:00:24 +0200

    Update ContentView

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertIsVibible: Bool = false
+    @State private var sliderValue: Double = 50.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text("89")
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: self.$sliderValue, in:1.0...100.0)
+                    Text("100")
+                        .bold()
+                }
+                Button(action: {
+                    print("Hello, SwiftUL!")
+                    self.alertIsVibible = true
+                }) {
+                    Text("HIT ME")
+                }
+                .alert(isPresented: $alertIsVibible, content: {
+                    var roundedValue: Int = Int(self.sliderValue.rounded())
+                    return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue)."), dismissButton: .default(Text("Awesome!")))
+                })
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
