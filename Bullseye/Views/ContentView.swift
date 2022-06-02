b07e5ba13cf52156d03a4b09bcf90df9b0c88209
Behnoosh <behnoosh.beheshti@gmail.com>
2022-06-02 14:09:38 +0200

    Extract slider and button view

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -11,49 +11,23 @@ struct ContentView: View {
     @State private var alertIsVibible = false
     @State private var sliderValue = 50.0
     @State private var game = Game()
+    
     var body: some View {
         ZStack {
             Color("BackgroundColor")
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
                 InstractionsView(game: $game)
-                HStack {
-                    Text("1")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                    Slider(value: $sliderValue, in:1.0...100.0)
-                    Text("100")
-                        .bold()
-                        .foregroundColor(Color("TextColor"))
-                }
-                .padding()
-                Button(action: {
-                    print("Hello, SwiftUL!")
-                    alertIsVibible = true
-                }) {
-                    Text("Hit me".uppercased())
-                        .bold()
-                        .font(.title3)
-                }
-                .padding(20.0)
-                .background(
-                    ZStack {
-                        Color("ButtonColor")
-                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-                    })
-                .foregroundColor(Color.white)
-                .cornerRadius(21.0)
-                .alert(isPresented: $alertIsVibible, content: {
-                    let roundedValue = Int(sliderValue.rounded())
-                    return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
-                })
-                
+                SliderView(sliderValue: $sliderValue)
+                HitMeButton(alertIsVibible: $alertIsVibible, sliderValue: $sliderValue, game: $game)
             }
         }
     }
 }
+
 struct InstractionsView: View {
     @Binding var game: Game
+    
     var body: some View {
         VStack {
             InstructionText(text: "🎯🎯🎯\n put the bulleye as close as you can to")
@@ -63,6 +37,48 @@ struct InstractionsView: View {
         }
     }
 }
+
+struct SliderView: View {
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: $sliderValue, in:1.0...100.0)
+            SliderLabelText(text: "100")
+        }.padding()
+    }
+}
+
+struct HitMeButton: View {
+    @Binding var alertIsVibible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View {
+        Button(action: {
+            print("Hello, SwiftUL!")
+            alertIsVibible = true
+        }) {
+            Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+            })
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .alert(isPresented: $alertIsVibible, content: {
+            let roundedValue = Int(sliderValue.rounded())
+            return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
+        })
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -75,3 +91,4 @@ struct ContentView_Previews: PreviewProvider {
             .previewLayout(.fixed(width: 568, height: 320))
     }
 }
+
