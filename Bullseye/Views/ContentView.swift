71862eb846387a165ba296cd8841e053eb3f5815
Behnoosh <behnoosh.beheshti@gmail.com>
2022-06-02 13:25:34 +0200

    Extract views

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -16,22 +16,15 @@ struct ContentView: View {
             Color("BackgroundColor")
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
-                Text("🎯🎯🎯\n put the bulleye as close as you can to".uppercased())
-                    .bold()
-                    .kerning(2.0)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(4.0)
-                    .font(.footnote)
-                Text(String(game.target))
-                    .kerning(-1.0)
-                    .font(.largeTitle)
-                    .fontWeight(.black)
+                InstractionsView(game: $game)
                 HStack {
                     Text("1")
                         .bold()
+                        .foregroundColor(Color("TextColor"))
                     Slider(value: $sliderValue, in:1.0...100.0)
                     Text("100")
                         .bold()
+                        .foregroundColor(Color("TextColor"))
                 }
                 .padding()
                 Button(action: {
@@ -59,11 +52,26 @@ struct ContentView: View {
         }
     }
 }
-
+struct InstractionsView: View {
+    @Binding var game: Game
+    var body: some View {
+        VStack {
+            InstructionText(text: "🎯🎯🎯\n put the bulleye as close as you can to")
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
+            BigNumberText(text: String(game.target))
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.dark)
             .previewLayout(.fixed(width: 568, height: 320))
     }
 }
