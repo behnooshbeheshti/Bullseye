4e5c377f4ca8e59db79e7ec3967dd0518b830bda
Behnoosh <behnoosh.beheshti@gmail.com>
2022-06-02 14:44:55 +0200

    Create shapes view

---
+++ Bullseye/Views/Shapes.swift
@@ -0,0 +1,20 @@
+//
+//  Shapes.swift
+//  Bullseye
+//
+//  Created by Mohsen Hossein pour on 6/2/22.
+//
+
+import SwiftUI
+
+struct Shapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Shapes_Previews: PreviewProvider {
+    static var previews: some View {
+        Shapes()
+    }
+}
