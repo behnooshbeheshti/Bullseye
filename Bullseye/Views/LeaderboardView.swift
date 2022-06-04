4571538d24e28453ee353544f9ac7758479ed66e
Behnoosh <behnoosh.beheshti@gmail.com>
2022-06-04 14:46:06 +0200

    Add different classes

--- Bullseye/Views/LeaderboardView.swift
+++ Bullseye/Views/LeaderboardView.swift
@@ -45,9 +45,19 @@ struct RowView: View {
 }
 
 struct HeaderView: View {
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     var body: some View {
         ZStack {
-            BigBoldText(text: ("Leaderboard"))
+            HStack {
+                if verticalSizeClass == .regular && horizontalSizeClass == .compact{
+                    BigBoldText(text: "Leaderboard")
+                        .padding(.leading)
+                    Spacer()
+                } else {
+                    BigBoldText(text: "Leaderboard")
+                }
+            }
             HStack {
                 Spacer()
                 Button(action: {}) {
