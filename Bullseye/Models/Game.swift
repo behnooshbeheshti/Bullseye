1701b0cddbc41f234dd1b630975e566c092d1b11
Behnoosh <behnoosh.beheshti@gmail.com>
2022-06-04 15:55:02 +0200

    Test leaderboard

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+struct LeaderboardEntry {
+    let score: Int
+    let date : Date
+}
+
 struct Game {
     var target = Int.random(in: 1...100)
     var score = 0
     var round = 1
+    var leaderboardEntries : [LeaderboardEntry] = []
     
     func points(sliderValue: Int) -> Int {
         let difference = abs(target - sliderValue)
@@ -26,10 +32,16 @@ struct Game {
         return 100 - difference + bonus
     }
     
+    mutating func addToLeaderboard(point: Int) {
+        leaderboardEntries.append(LeaderboardEntry(score: point, date: Date()))
+        leaderboardEntries.sort { $0.score > $1.score}
+    }
+    
     mutating func startNewRound(points: Int) {
         score += points
         round += 1
         target = Int.random(in: 1...100)
+        addToLeaderboard(point: points)
     }
     
     mutating func restart() {
